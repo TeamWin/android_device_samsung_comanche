e039cd85678f915235c79c36c3e57a632c7e80b8
Mike C <mcrosson_git at nusku dot net>
2012-11-18 22:47:33 -0500

    Reconcile with D2 (same platform) and update recovery bits

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,28 +1,46 @@
+# WARNING: This line must come *before* including the proprietary
+# variant, so that it gets overwritten by the parent (which goes
+# against the traditional rules of inheritance).
 USE_CAMERA_STUB := true
+
+# inherit from common msm8960
+-include device/samsung/msm8960-common/BoardConfigCommon.mk
 
 # inherit from the proprietary version
 -include vendor/samsung/comanche/BoardConfigVendor.mk
 
-TARGET_NO_BOOTLOADER := true
-TARGET_BOARD_PLATFORM := unknown
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_BOOTLOADER_BOARD_NAME := comanche
-
-BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom user_debug=31
-BOARD_KERNEL_BASE := 0x80200000
-BOARD_KERNEL_PAGESIZE := 2048
+# Kernel
+#TARGET_PREBUILT_KERNEL      := device/samsung/comanche/kernel
+TARGET_KERNEL_SOURCE        := kernel/samsung/comanche_samsung
+TARGET_KERNEL_CONFIG        := comanche_rev02_defconfig
+BOARD_KERNEL_CMDLINE        := console=null androidboot.hardware=qcom user_debug=31
+BOARD_KERNEL_BASE           := 0x80200000
 BOARD_FORCE_RAMDISK_ADDRESS := 0x81500000
+BOARD_KERNEL_PAGESIZE       := 2048
 
+TARGET_BOOTLOADER_BOARD_NAME := MSM8960
 
-BOARD_BOOTIMAGE_PARTITION_SIZE := 10485760
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 10485760
+# Recovery
+BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/comanche/recovery/recovery_keys.c
+BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_15x24.h\"
+BOARD_USES_MMCUTILS := true
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_HAS_NO_MISC_PARTITION := true
+BOARD_HAS_NO_SELECT_BUTTON := true
+TARGET_RECOVERY_FSTAB := device/samsung/comanche/recovery.fstab
+
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00A00000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00A00000
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1258291200
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 5498732544
 BOARD_FLASH_BLOCK_SIZE := 131072
 
-TARGET_PREBUILT_KERNEL := device/samsung/comanche/kernel
-TARGET_RECOVERY_INITRC := device/samsung/comanche/recovery/init.rc
-BOARD_CUSTOM_GRAPHICS:= ../../../device/samsung/comanche/recovery/minui/graphics.c
+# Disable initlogo, Samsungs framebuffer is weird
+TARGET_NO_INITLOGO := true
 
-BOARD_HAS_NO_SELECT_BUTTON := true
-BOARD_HAS_LARGE_FILESYSTEM := true
+# Use Audience A2220 chip
+BOARD_HAVE_AUDIENCE_A2220 := true
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := comanche
