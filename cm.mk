3e957a7f008c053276657c42113be0cac4a30625
Mike C <mcrosson_git at nusku dot net>
2012-11-19 20:46:34 -0500

    General merge with d2att and d2common (bugs with lunch)

--- cm.mk
+++ cm.mk
@@ -1,18 +1,16 @@
-## Specify phone tech before including full_phone
+$(call inherit-product, device/samsung/comanche/full_comanche.mk)
+
+# Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
-# Release name
-PRODUCT_RELEASE_NAME := comanche
+# Enhanced NFC
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-# Inherit device configuration
-$(call inherit-product, device/samsung/comanche/device_comanche.mk)
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=commanche TARGET_DEVICE=comanche BUILD_FINGERPRINT="comanche-user 4.0.4 IMM76D I547UCALEM release-keys" PRIVATE_BUILD_DESC="samsung/comanche:4.0.4/IMM76D/I547UCALEM:user/release-keys"
 
-## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := comanche
 PRODUCT_NAME := cm_comanche
-PRODUCT_BRAND := samsung
-PRODUCT_MODEL := comanche
-PRODUCT_MANUFACTURER := samsung
+PRODUCT_DEVICE := comanche 
+
---
+++ device.mk
@@ -0,0 +1,86 @@
+$(call inherit-product-if-exists, vendor/samsung/comanche/comanche-vendor.mk)
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+## The gps config appropriate for this device
+PRODUCT_COPY_FILES += device/common/gps/gps.conf_US_SUPL:system/etc/gps.conf
+
+## overlays
+DEVICE_PACKAGE_OVERLAYS += device/samsung/comanche/overlay
+
+# Device uses high-density artwork where available
+PRODUCT_AAPT_CONFIG := normal hdpi
+PRODUCT_AAPT_PREF_CONFIG := hdpi
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 800
+TARGET_SCREEN_WIDTH := 480
+
+# Audio configuration
+PRODUCT_COPY_FILES += \
+    device/samsung/comanche/audio/snd_soc_msm_2x:system/etc/snd_soc_msm/snd_soc_msm_2x \
+    device/samsung/comanche/audio/audio_policy.conf:system/etc/audio_policy.conf
+
+# Ramdisk
+PRODUCT_PACKAGES += \
+    fstab.qcom \
+    initlogo.rle \
+    init.bt.rc \
+    init.qcom.rc \
+    init.qcom.usb.rc \
+    init.target.rc \
+    ueventd.qcom.rc
+
+# Torch
+PRODUCT_PACKAGES += Torch
+
+# Vold configuration
+PRODUCT_COPY_FILES += \
+    device/samsung/comanche/vold.fstab:system/etc/vold.fstab
+
+# Set default USB interface
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp
+
+# Lights
+PRODUCT_PACKAGES += lights.msm8960
+
+# QC Perf
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.extension_library=/system/lib/libqc-opt.so
+
+# QRNGD
+PRODUCT_PACKAGES += qrngd
+
+#common build.props
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.ril.hsxpa=1 \
+    ro.ril.gprsclass=10 \
+    persist.radio.add_power_save=1 \
+    persist.radio.snapshot_disabled=1 \
+    com.qc.hardware=true \
+    persist.radio.apm_sim_not_pwdn=1 \
+    persist.gps.qmienabled=true \
+    ro.telephony.call_ring.multiple=0 \
+    ro.sf.lcd_density=320 \
+    ro.ril.transmitpower=true \
+    ro.opengles.version=131072 \
+    persist.audio.fluence.mode=endfire \
+    persist.audio.vr.enable=false \
+    persist.audio.handset.mic=digital \
+    persist.audio.speaker.location=high \
+    ro.qc.sdk.audio.fluencetype=fluence \
+    persist.timed.enable=true \
+    ro.emmc.sdcard.partition=17 \
+    ro.use_data_netmgrd=true \
+    persist.data_netmgrd_nint=16 \
+    lpa.decode=true \
+    rild.libpath=/system/lib/libril-qc-qmi-1.so \
+    ril.subscription.types=NV,RUIM \
+    ro.telephony.ril.v3=skipnullaid
+
+# common msm8960
+$(call inherit-product, device/samsung/msm8960-common/msm8960.mk)
+
+# Device vendor
+$(call inherit-product-if-exists, vendor/samsung/comanche/comanche-vendor.mk)
