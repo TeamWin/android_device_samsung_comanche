fce7a926c44dc4541ffca68d2ab38b72088494b5
Mike C <mcrosson_git at nusku dot net>
2012-11-19 20:56:01 -0500

    Fixups

--- cm.mk
+++ cm.mk
@@ -9,7 +9,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=commanche TARGET_DEVICE=comanche BUILD_FINGERPRINT="comanche-user 4.0.4 IMM76D I547UCALEM release-keys" PRIVATE_BUILD_DESC="samsung/comanche:4.0.4/IMM76D/I547UCALEM:user/release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-I547 TARGET_DEVICE=comanche BUILD_FINGERPRINT="comanche-user 4.0.4 IMM76D I547UCALEM release-keys" PRIVATE_BUILD_DESC="samsung/comanche:4.0.4/IMM76D/I547UCALEM:user/release-keys"
 
 PRODUCT_NAME := cm_comanche
 PRODUCT_DEVICE := comanche 
