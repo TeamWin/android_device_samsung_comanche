5e4d727703df5801956ce1df08c945231339d392
Mike C <mcrosson_git at nusku dot net>
2012-11-21 19:01:24 +0000

    TWRP / USB fixups / recovery using "correct" mount points for sd card and external sd

--- device.mk
+++ device.mk
@@ -46,6 +46,7 @@ PRODUCT_COPY_FILES += \
 # Set default USB interface
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     persist.sys.usb.config=mtp \
+    ro.debuggable=1 \
     persist.service.adb.enable=1
 
 # Lights
